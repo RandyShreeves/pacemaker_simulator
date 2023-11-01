d503d78b5e019f795332787795e601005d7a2ca9
Randy Shreeves <114593302+RandyShreeves@users.noreply.github.com>
2023-11-01 09:44:28 -0400

    Add documentation and setup workspace
    Updated README file and setup workspace. Added documentation comments
    to pacemaker_simulator.c file.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # pacemaker_simulator
-Program that simulates a pacemaker by energizing different LEDs
+Program that simulates a pacemaker by energizing different LEDs.
+
+This system uses the TM4C123G microcontroller, a button, and LEDs to simulate a pacemaker.
+A green LED is energized when the system is ready to deliver a simulated shock.
+The button represents an atrial sensor. When it's pushed it simulates sensing a heart's atrial contraction by de-energizing the green LED.
+When the button is released, it represents the completion of the atrial contraction.
+The system waits 250ms for blood to flow from the heart's atria to its ventricles.
+Then a red LED energizes for 250ms, which simulates a shock to the ventricles.
+Once the simulated shock is delivered, the ventricles pump blood to the rest of the body. Then the green LED returns to the ready state.
---
+++ pacemaker_simulator.c
@@ -0,0 +1,17 @@
+// 0. Documentation Section
+// This program simulates a pacemaker by energizing different LEDs.
+// This program uses the TM4C123G microcontroller, a button, and LEDs to simulate a pacemaker.
+// A green LED is energized when the system is ready to deliver a simulated shock.
+// The button represents an atrial sensor. When it's pushed it simulates sensing a heart's atrial contraction by de-energizing the green LED.
+// When the button is released, it represents the completion of the atrial contraction.
+// The system waits 250ms for blood to flow from the heart's atria to its ventricles.
+// Then a red LED energizes for 250ms, which simulates a shock to the ventricles.
+// Once the simulated shock is delivered, the ventricles pump blood to the rest of the body. Then the green LED returns to the ready state.
+// Author: Randy Shreeves
+// Date: 11/01/2023
+
+// 1. Pre-processor Directives Section
+
+// 2. Global Declarations Section
+
+// 3. Subroutines Section
